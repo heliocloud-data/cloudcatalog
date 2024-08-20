45617b76b3d28830fa0bdc46f6ec1192a46d2b80
antunak1 <sandy.antunes@jhuapl.edu>
2024-08-20 11:29:56 -0400

    updated test to match fixed code

--- tests/test_hdrl.py
+++ tests/test_hdrl.py
@@ -11,7 +11,7 @@ mmsstart = '2020-02-01T00:00:00Z'
 mmsstop =   '2020-02-02T00:00:00Z'
 aiaid = "aia_0094"
 aiastart="2010-05-13T00:00:00Z"
-aiastop="2010-12-31T23:56:00Z"
+aiastop="2010-06-30T23:56:00Z"
 euvid = "euvml_stereoa_171"
 euvstart="2018-05-13T00:00:00Z"
 euvstop="2018-12-31T23:56:00Z"
@@ -19,12 +19,15 @@ euvstop="2018-12-31T23:56:00Z"
 fr=cloudcatalog.CloudCatalog("s3://gov-nasa-hdrl-data1/",cache=False)
 
 filekeys_mms = fr.request_cloud_catalog(mmsid,start_date=mmsstart,stop_date=mmsstop)
-assert len(filekeys_mms) == 8954
+#print(len(filekeys_mms))
+assert len(filekeys_mms) == 15
 
 filekeys_aia = fr.request_cloud_catalog(aiaid,start_date=aiastart,stop_date=aiastop)
-assert len(filekeys_aia) == 81034
+#print(len(filekeys_aia))
+assert len(filekeys_aia) == 16783
 
 filekeys_euv = fr.request_cloud_catalog(euvid,start_date=euvstart,stop_date=euvstop)
+#print(len(filekeys_euv))
 assert len(filekeys_euv) == 780
 
 mysearch = cloudcatalog.EntireCatalogSearch()
