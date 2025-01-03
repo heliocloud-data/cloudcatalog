d1f97f06556baeb38826f1d43e324bae77fc9880
Knowles, Lisa A <Lisa.Knowles@jhuapl.edu>
2025-01-03 14:02:49 -0500

    First draft of completed validator script
    Finished the validator script so it should do what we set out for it to do. Plenty of updates and code review still to be done. Committing this as a first draft.

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -4,6 +4,11 @@ import re
 
 catalogname = "s3://gov-nasa-hdrl-data1/catalog.json"
 
+def valid_endpoint(collectionendpoint):
+    if not re.search('^[s3://|https://].*/$', collectionendpoint): # <----- SPEC SAYS https:// OR EQUIVALENT????
+        return False
+    else:
+        return True
 
 def valid_id(datasetid):
     if not re.search(r'^[a-zA-Z0-9_-]*$', datasetid):
@@ -12,13 +17,14 @@ def valid_id(datasetid):
         return True
 
 def valid_index(filepath):
-    if not re.search('^[s3://|https://].*/$', filepath): # <----- SPEC SAYS https:// OR EQUIVALENT????
+    if not re.search('^[s3://|https://].*/.*/$', filepath): # <----- SPEC SAYS https:// OR EQUIVALENT????
         return False
     else:
         return True
 
+# ENCOUNTERED AN ISSUE HERE WHERE SOME SECONDS ARE IN 00:00 AND SOME ARE IN 00.00. ARE BOTH VALID OR IS ONLY ONE VALID?
 def valid_start(startstr):
-    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}\.\d{2}Z', startstr):
+    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(\.|\:)\d{2}Z', startstr):
         return False
     else:
         return True
@@ -43,8 +49,9 @@ def valid_indextype(indextype):
     else:
         return True
 
+#I'M TELLING IT TO IGNORE THE CASE I HOPE THATS OK
 def valid_filetype(filetype):
-    if not re.search('^(fits|csv|cdf|netcdf3|netcdf4|hdf5|datamap|txt|binary|other)$', filetype):
+    if not re.search('^(fits|csv|cdf|netcdf3|netcdf4|hdf5|datamap|txt|binary|other)$', filetype, re.IGNORECASE):
         return False
     else:
         return True
@@ -85,83 +92,127 @@ def valid_multiyear(multiyearboo):
 
 def validator(catalogname):
     with open(catalogname, "r") as fin:
-        catalog = json.load(fin)
+        topcatalog = json.load(fin)
 
-    if not catalog['id']:
-        print('No entry for "id" provided! Dataset ID is a required catalog entry!')
+    if 'name' not in topcatalog:
+        print('No entry for "name" provided! Collection name is a required entry! Name should be a descriptive '
+              'title for the collection of datasets. Should be the same as provided to GlobalDataRegistry.json.')
+
+    if 'endpoint' not in topcatalog:
+        print('No entry for "endpoint" provided! Collection endpoint is a required entry!')
     else:
-        if not valid_id(catalog['id']):
-            print('Invalid dataset ID. ID can only contain alphanumeric characters, dashes, or underscores. No spaces'
-                  ' or any other characters.')
+        if not valid_endpoint(topcatalog['endpoint']):
+            print('Invalid endpoint. Endpoint must be a an accessible S3 (or equivalent) bucket link. It must start '
+                  'with "s3://" or "https://" and end in a terminating "/".')
 
-    if not catalog['index']:
-        print('No entry for "index" provided! Dataset index is a required catalog entry!')
-    else:
-        if not valid_index(catalog['index']):
-            print('Invalid index. Index must start with "s3://" or "https://" and end in a terminating "/".')
+    if 'egress' not in topcatalog:
+        print('No entry for "egress" provided! Egress should be a set of keywords defining permissions. Currently '
+              'optional but will be required in the near future.')
 
-    if not catalog['start']:
-        print('No entry for "start" provided! Start date is a required catalog entry!')
-    else:
-        if not valid_start(catalog['start']):
-            print('Invalid start date. Start date must be a date/time string in Restricted ISO 8601 format.')
-            # INCLUDE EXAMPLE OF DATE FORMAT? I.E. YYYY-MM-DDTHH:MM.SSZ?
+    if 'status' not in topcatalog:
+        print('No entry for "status" provided! Status is a return code and should communicate if a dataset is '
+              'temporarily down or has other constraints. Default is "code: 1200, message: OK".')
 
-    if not catalog['stop']:
-        print('No entry for "stop" provided! Stop date is a required catalog entry!')
-    else:
-        if not valid_stop(catalog['stop']):
-            print('Invalid stop date. Stop date must be a date/time string in Restricted ISO 8601 format.')
-
-    if not catalog['modification']:
-        print('No entry for "modification" provided! Modification date is a required catalog entry!')
-    else:
-        if not valid_modification(catalog['modification']):
-            print('Invalid modification date. Modification date must be a date/time string in Restricted ISO 8601 '
-                  'format.')
-
-    if not catalog['title']:
-        print('No entry for "title" provided! Dataset title is a required catalog entry!')
-    # SHOULD I ADD SOMETHING HERE FOR THE TITLE ENTRY????
-
-    if not catalog['indextype']:
-        print('No entry for "indextype" provided! Index type is a required catalog entry!')
-    else:
-        if not valid_indextype(catalog['indextype']):
-            print('Invalid entry for "indextype." Index type can be either csv, csv-zip, or parquet.')
-
-    if not catalog['filetype']:
-        print('No entry for "filetype" provided! File type is a required catalog entry!')
-    else:
-        if not valid_filetype(catalog['filetype']):
-            print('Invalid entry for "filetype." Current permitted types are fits, csv, cdf, netcdf3, netcdf4, hdf5, '
-                  'datamap, txt, binary, and other.')
+    if 'contact' not in topcatalog:
+        print('No entry for "contact" provided! Contact should be the name and email address of the person to contact '
+              'with issues with the collection.')
 
     # OPTIONAL DESCRIPTION
 
-    # OPTIONAL RESOURCE
-
-    if catalog['creation'] and not valid_creation(catalog['creation']):
-        print('Invalid entry for "creation." Creation date must be a date/time string in ISO 8601 format.')
-
-    if catalog['expiration'] and not valid_expiration(catalog['expiration']):
-        print('Invalid entry for "expiration." Expiration date must be a date/time string in ISO 8601 format.')
-
-    if catalog['verified'] and not valid_verified(catalog['verified']):
-        print('Invalid entry for "verified." Verified date must be a date/time string in ISO 8601 format.')
-
     # OPTIONAL CITATION
 
-    # OPTIONAL CONTACT NAME
+    # OPTIONAL COMMENT
 
-    # OPTIONAL ABOUT
+    datasetcatalog = topcatalog['catalog']
 
-    if catalog['multiyear'] and not valid_multiyear(catalog['multiyear']):
-        print('Invalid entry for "multiyear." Multiyear entry must either be "True" or "False."')
+    datalength = str(len(datasetcatalog))
+
+    for idx, catalog in enumerate(datasetcatalog):
+        print("Validating dataset #" + str(idx + 1) + " of " + datalength)
+
+        if 'id' not in catalog:
+            print('No entry for "id" provided! Dataset ID is a required catalog entry!')
+        else:
+            if not valid_id(catalog['id']):
+                print('Invalid dataset ID. ID can only contain alphanumeric characters, dashes, or underscores. No'
+                      'spaces or any other characters.')
+
+        if 'index' not in catalog:
+            print('No entry for "index" provided! Dataset index is a required catalog entry!')
+        else:
+            if not valid_index(catalog['index']):
+                print('Invalid index. Index must be a pointed to the object directory and contain the dataset name. It '
+                      'must start with "s3://" or "https://" and end in a terminating "/".')
+
+        if 'start' not in catalog:
+            print('No entry for "start" provided! Start date is a required catalog entry!')
+        else:
+            if not valid_start(catalog['start']):
+                print('Invalid start date. Start date must be a date/time string in Restricted ISO 8601 format.')
+                # INCLUDE EXAMPLE OF DATE FORMAT? I.E. YYYY-MM-DDTHH:MM.SSZ?
+
+        if 'stop' not in catalog:
+            print('No entry for "stop" provided! Stop date is a required catalog entry!')
+        else:
+            if not valid_stop(catalog['stop']):
+                print('Invalid stop date. Stop date must be a date/time string in Restricted ISO 8601 format.')
+
+        if 'modification' not in catalog:
+            print('No entry for "modification" provided! Modification date is a required catalog entry!')
+        else:
+            if not valid_modification(catalog['modification']):
+                print('Invalid modification date. Modification date must be a date/time string in Restricted ISO 8601 '
+                      'format.')
+
+        if 'title' not in catalog:
+            print('No entry for "title" provided! Dataset title is a required catalog entry!')
+        # SHOULD I ADD SOMETHING HERE FOR THE TITLE ENTRY????
+
+        if 'indextype' not in catalog:
+            print('No entry for "indextype" provided! Index type is a required catalog entry!')
+        else:
+            if not valid_indextype(catalog['indextype']):
+                print('Invalid entry for "indextype." Index type can be either csv, csv-zip, or parquet.')
+
+        if 'filetype' not in catalog:
+            print('No entry for "filetype" provided! File type is a required catalog entry!')
+        else:
+            if not valid_filetype(catalog['filetype']):
+                print('Invalid entry for "filetype." Current permitted types are fits, csv, cdf, netcdf3, netcdf4, hdf5, '
+                      'datamap, txt, binary, and other.')
+
+        # OPTIONAL DESCRIPTION
+
+        # OPTIONAL RESOURCE
+
+        if 'creation' not in catalog:
+            pass
+        elif catalog['creation'] and not valid_creation(catalog['creation']):
+            print('Invalid entry for "creation." Creation date must be a date/time string in ISO 8601 format.')
+
+        if 'expiration' not in catalog:
+            pass
+        elif catalog['expiration'] and not valid_expiration(catalog['expiration']):
+            print('Invalid entry for "expiration." Expiration date must be a date/time string in ISO 8601 format.')
+
+        if 'verified' not in catalog:
+            pass
+        elif catalog['verified'] and not valid_verified(catalog['verified']):
+            print('Invalid entry for "verified." Verified date must be a date/time string in ISO 8601 format.')
+
+        # OPTIONAL CITATION
+
+        # OPTIONAL CONTACT NAME
+
+        # OPTIONAL ABOUT
+
+        if 'multiyear' not in catalog:
+            pass
+        elif catalog['multiyear'] and not valid_multiyear(catalog['multiyear']):
+            print('Invalid entry for "multiyear." Multiyear entry must either be "True" or "False."')
 
 
 validator(catalogname)
-
 
 if __name__ == 'main':
     validator(catalogname)
