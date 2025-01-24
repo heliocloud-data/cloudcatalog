4abbe09bc29ad902d55fcaec92d7224955bc2a0a
Knowles, Lisa A <Lisa.Knowles@jhuapl.edu>
2025-01-24 15:24:01 -0500

    Update validator.py
    Final merge to json validator branch before merging with develop branch

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,14 +1,22 @@
+"""
+Validator for new entries to the CloudCatalog. Returns an error for catalog metadata that doesn't match the spec.
+
+Find the spec here: https://gitlab.smce.nasa.gov/heliocloud/scregistry/-/blob/develop/docs/cloudcatalog-spec.md?ref_type=heads
+
+Contact Lisa Knowles lisa.knowles@jhuapl.edu
+"""
+
 import json
 from smart_open import open
 import re
 
-catalogname = "s3://gov-nasa-hdrl-data1/catalog.json"
 
 def valid_endpoint(collectionendpoint):
-    if not re.search('^[s3://|https://].*/$', collectionendpoint): # <----- SPEC SAYS https:// OR EQUIVALENT????
+    if not re.search('^[s3://|https://].*/$', collectionendpoint):
         return False
     else:
         return True
+
 
 def valid_id(datasetid):
     if not re.search(r'^[a-zA-Z0-9_-]*$', datasetid):
@@ -16,79 +24,76 @@ def valid_id(datasetid):
     else:
         return True
 
+
 def valid_index(filepath):
-    if not re.search('^[s3://|https://].*/.*/$', filepath): # <----- SPEC SAYS https:// OR EQUIVALENT????
+    if not re.search('^[s3://|https://].*/.*/$', filepath):
         return False
     else:
         return True
 
-# ENCOUNTERED AN ISSUE HERE WHERE SOME SECONDS ARE IN 00:00 AND SOME ARE IN 00.00. ARE BOTH VALID OR IS ONLY ONE VALID?
+
 def valid_start(startstr):
-    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(\.|\:)\d{2}Z', startstr):
+    if not re.fullmatch(r'\d{4}-\d{2}-\d{2}T\d{2}(:\d{2}(:\d{2}(\.\d+)?)?)?Z', startstr):
         return False
     else:
         return True
+
 
 def valid_stop(stopstr):
-    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}\.\d{2}Z', stopstr):
+    if not re.fullmatch(r'\d{4}-\d{2}-\d{2}T\d{2}(:\d{2}(:\d{2}(\.\d+)?)?)?Z', stopstr):
         return False
     else:
         return True
+
 
 def valid_modification(modstr):
-    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}\.\d{2}Z', modstr):
+    if not re.fullmatch(r'\d{4}-\d{2}-\d{2}T\d{2}(:\d{2}(:\d{2}(\.\d+)?)?)?Z', modstr):
         return False
     else:
         return True
 
-# SHOULD I ADD SOMETHING HERE FOR THE TITLE ENTRY????
 
 def valid_indextype(indextype):
-    if not re.search('^(csv|csv-zip|parquet)$', indextype):
+    if not re.search('^(csv|csv-zip|parquet)$', indextype, re.IGNORECASE):
         return False
     else:
         return True
 
-#I'M TELLING IT TO IGNORE THE CASE I HOPE THATS OK
+
 def valid_filetype(filetype):
     if not re.search('^(fits|csv|cdf|netcdf3|netcdf4|hdf5|datamap|txt|binary|other)$', filetype, re.IGNORECASE):
         return False
     else:
         return True
 
-# OPTIONAL DESCRIPTION
-
-# OPTIONAL RESOURCE IDENTIFIER
 
 def valid_creation(creationstr):
-    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}\.\d{2}Z', creationstr):
+    if not re.fullmatch(r'\d{4}-\d{2}-\d{2}T\d{2}(:\d{2}(:\d{2}(\.\d+)?)?)?Z', creationstr):
         return False
     else:
         return True
+
 
 def valid_expiration(expstr):
-    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}\.\d{2}Z', expstr):
+    if not re.fullmatch(r'\d{4}-\d{2}-\d{2}T\d{2}(:\d{2}(:\d{2}(\.\d+)?)?)?Z', expstr):
         return False
     else:
         return True
+
 
 def valid_verified(verifstr):
-    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}\.\d{2}Z', verifstr):
+    if not re.search(r'\d{4}-\d{2}-\d{2}T\d{2}(:\d{2}(:\d{2}(\.\d+)?)?)?Z', verifstr):
         return False
     else:
         return True
 
-# OPTIONAL CITATION
-
-# OPTIONAL CONTACT NAME
-
-# OPTIONAL ABOUT
 
 def valid_multiyear(multiyearboo):
-    if not re.search('^(True|False)$', multiyearboo): # DOES CAPITALIZATION MATTER????
+    if not re.search('^(True|False)$', multiyearboo, re.IGNORECASE):
         return False
     else:
         return True
+
 
 def validator(catalogname):
     with open(catalogname, "r") as fin:
@@ -117,12 +122,6 @@ def validator(catalogname):
         print('No entry for "contact" provided! Contact should be the name and email address of the person to contact '
               'with issues with the collection.')
 
-    # OPTIONAL DESCRIPTION
-
-    # OPTIONAL CITATION
-
-    # OPTIONAL COMMENT
-
     datasetcatalog = topcatalog['catalog']
 
     datalength = str(len(datasetcatalog))
@@ -148,25 +147,25 @@ def validator(catalogname):
             print('No entry for "start" provided! Start date is a required catalog entry!')
         else:
             if not valid_start(catalog['start']):
-                print('Invalid start date. Start date must be a date/time string in Restricted ISO 8601 format.')
-                # INCLUDE EXAMPLE OF DATE FORMAT? I.E. YYYY-MM-DDTHH:MM.SSZ?
+                print('Invalid start date. Start date must be a date/time string in Restricted ISO 8601 format: '
+                      'XXXX-XX-XXTXXZ with at least the year, month, day, and hour specified.')
 
         if 'stop' not in catalog:
             print('No entry for "stop" provided! Stop date is a required catalog entry!')
         else:
             if not valid_stop(catalog['stop']):
-                print('Invalid stop date. Stop date must be a date/time string in Restricted ISO 8601 format.')
+                print('Invalid stop date. Stop date must be a date/time string in Restricted ISO 8601 format: '
+                      'XXXX-XX-XXTXXZ with at least the year, month, day, and hour specified.')
 
         if 'modification' not in catalog:
             print('No entry for "modification" provided! Modification date is a required catalog entry!')
         else:
             if not valid_modification(catalog['modification']):
                 print('Invalid modification date. Modification date must be a date/time string in Restricted ISO 8601 '
-                      'format.')
+                      'format: XXXX-XX-XXTXXZ with at least the year, month, day, and hour specified.')
 
         if 'title' not in catalog:
             print('No entry for "title" provided! Dataset title is a required catalog entry!')
-        # SHOULD I ADD SOMETHING HERE FOR THE TITLE ENTRY????
 
         if 'indextype' not in catalog:
             print('No entry for "indextype" provided! Index type is a required catalog entry!')
@@ -178,12 +177,8 @@ def validator(catalogname):
             print('No entry for "filetype" provided! File type is a required catalog entry!')
         else:
             if not valid_filetype(catalog['filetype']):
-                print('Invalid entry for "filetype." Current permitted types are fits, csv, cdf, netcdf3, netcdf4, hdf5, '
-                      'datamap, txt, binary, and other.')
-
-        # OPTIONAL DESCRIPTION
-
-        # OPTIONAL RESOURCE
+                print('Invalid entry for "filetype." Current permitted types are fits, csv, cdf, netcdf3, netcdf4, '
+                      'hdf5, datamap, txt, binary, and other.')
 
         if 'creation' not in catalog:
             pass
@@ -193,29 +188,19 @@ def validator(catalogname):
         if 'expiration' not in catalog:
             pass
         elif catalog['expiration'] and not valid_expiration(catalog['expiration']):
-            print('Invalid entry for "expiration." Expiration date must be a date/time string in ISO 8601 format.')
+            print('Invalid entry for "expiration." Expiration date must be a date/time string in ISO 8601 format: '
+                  'XXXX-XX-XXTXXZ with at least the year, month, day, and hour specified.')
 
         if 'verified' not in catalog:
             pass
         elif catalog['verified'] and not valid_verified(catalog['verified']):
-            print('Invalid entry for "verified." Verified date must be a date/time string in ISO 8601 format.')
-
-        # OPTIONAL CITATION
-
-        # OPTIONAL CONTACT NAME
-
-        # OPTIONAL ABOUT
+            print('Invalid entry for "verified." Verified date must be a date/time string in ISO 8601 format: '
+                  'XXXX-XX-XXTXXZ with at least the year, month, day, and hour specified.')
 
         if 'multiyear' not in catalog:
             pass
         elif catalog['multiyear'] and not valid_multiyear(catalog['multiyear']):
             print('Invalid entry for "multiyear." Multiyear entry must either be "True" or "False."')
 
-
-validator(catalogname)
-
-if __name__ == 'main':
-    validator(catalogname)
-
-
-
+# if __name__ == 'main':
+#    validator(catalogname)
